efb44a7cf0806e92cd085b8338486f2caf89d339
Aleksandr <kovalyov.lex@gmail.com>
2025-10-09 15:41:08 +0100

    Trying to use another heap use after free races

--- async_coro/include/async_coro/internal/task_handle_awaiter.h
+++ async_coro/include/async_coro/internal/task_handle_awaiter.h
@@ -28,7 +28,9 @@ class task_handle_awaiter {
   task_handle_awaiter(const task_handle_awaiter&) = delete;
   task_handle_awaiter(task_handle_awaiter&&) = delete;
 
-  ~task_handle_awaiter() noexcept = default;
+  ~task_handle_awaiter() noexcept {
+    _can_be_freed.wait(true, std::memory_order::acquire);
+  }
 
   task_handle_awaiter& operator=(const task_handle_awaiter&) = delete;
   task_handle_awaiter& operator=(task_handle_awaiter&&) = delete;
@@ -38,7 +40,8 @@ class task_handle_awaiter {
   template <typename T>
     requires(std::derived_from<T, base_handle>)
   void await_suspend(std::coroutine_handle<T> handle) {
-    _was_done.store(false, std::memory_order::relaxed);
+    _can_be_freed.store(false, std::memory_order::relaxed);
+    _was_done.store(false, std::memory_order::release);
 
     _suspension = handle.promise().suspend(2, &_on_cancel_callback);
 
@@ -52,9 +55,16 @@ class task_handle_awaiter {
   }
 
  private:
+  void set_can_be_freed() noexcept {
+    if (!_can_be_freed.exchange(true, std::memory_order::release)) {
+      _can_be_freed.notify_one();
+    }
+  }
+
+ private:
   struct on_cancel_callback {
     void operator()() const {
-      if (!clb->_was_done.exchange(true, std::memory_order::relaxed)) {
+      if (!clb->_was_done.exchange(true, std::memory_order::acquire)) {
         // cancel
         clb->_suspension.try_to_continue_from_any_thread(true);
       }
@@ -63,21 +73,40 @@ class task_handle_awaiter {
     task_handle_awaiter* clb;
   };
 
-  struct on_continue_callback {
-    void operator()(promise_result<R>& /*result*/, bool canceled) const {
+  class on_continue_callback : public callback<void(promise_result<R>&, bool)> {
+    using super = callback<void(promise_result<R>&, bool)>;
+
+   public:
+    explicit on_continue_callback(task_handle_awaiter* awaiter) noexcept
+        : super(&executor, &deleter),
+          _awaiter(awaiter) {}
+
+   private:
+    static void executor(callback_base* base, ASYNC_CORO_ASSERT_VARIABLE bool with_destroy, promise_result<R>& /*result*/, bool cancelled) {
+      ASYNC_CORO_ASSERT(with_destroy);
+
+      auto* clb = static_cast<on_continue_callback*>(base)->_awaiter;
+
       if (!clb->_was_done.exchange(true, std::memory_order::relaxed)) {
-        clb->_suspension.try_to_continue_from_any_thread(canceled);
+        clb->set_can_be_freed();
+        clb->_suspension.try_to_continue_from_any_thread(cancelled);
       }
     }
 
-    task_handle_awaiter* clb;
+    static void deleter(callback_base* base) noexcept {
+      static_cast<on_continue_callback*>(base)->_awaiter->set_can_be_freed();
+    }
+
+   private:
+    task_handle_awaiter* _awaiter;
   };
 
  private:
+  std::atomic_bool _can_be_freed{true};
   task_handle<R> _th;
   coroutine_suspender _suspension;
   callback_on_stack<on_cancel_callback, void()> _on_cancel_callback;
-  callback_on_stack<on_continue_callback, void(promise_result<R>&, bool)> _on_continue_callback;
+  on_continue_callback _on_continue_callback;
   std::atomic_bool _was_done{false};
 };
 
