bc9e6a047417157c6d569fd25cf532a17946d582
Aleksandr <kovalyov.lex@gmail.com>
2026-02-03 19:19:40 +0000

    more logs

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -14,6 +14,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 
 // NOLINTBEGIN(*array-index*, *vararg)
 
@@ -26,6 +27,20 @@ static void write_all(int fd, const char *buf, size_t len) {
     buf += n;
     len -= n;
   }
+}
+
+static void log_message(const char *msg) {
+  std::array<char, 256> timestamp_buf{};
+  time_t now = time(nullptr);
+  struct tm *tm_info = localtime(&now);
+  strftime(timestamp_buf.data(), timestamp_buf.size(), "%Y-%m-%d %H:%M:%S", tm_info);
+
+  std::array<char, 512> full_msg{};
+  const auto pid = getpid();
+  const auto tid = (pid_t)syscall(SYS_gettid);
+  snprintf(full_msg.data(), full_msg.size(), "[%s] PID=%d TID=%d: %s\n",
+           timestamp_buf.data(), (int)pid, (int)tid, msg);
+  write_all(STDERR_FILENO, full_msg.data(), strlen(full_msg.data()));
 }
 
 static void print_backtrace_to_stderr() {
@@ -62,30 +77,32 @@ static void print_backtrace_to_stderr() {
 }
 
 static void sigusr2_handler(int /*signo*/, siginfo_t * /*info*/, void * /*ucontext*/) {
-  std::array<char, 128> hdr{};
-
+  std::array<char, 256> hdr{};
   const auto tid = (pid_t)syscall(SYS_gettid);
-  int n = snprintf(hdr.data(), hdr.size(), "=== Backtrace for thread %d ===\n", (int)tid);
+  int n = snprintf(hdr.data(), hdr.size(), "=== [RECEIVED SIGUSR2] Backtrace for thread %d ===", (int)tid);
   if (n > 0) {
+    write_all(STDERR_FILENO, "\n", 1);
     write_all(STDERR_FILENO, hdr.data(), (size_t)n);
+    write_all(STDERR_FILENO, "\n", 1);
   }
   print_backtrace_to_stderr();
 }
 
-static void install_backtrace_handler() {
-  struct sigaction sa{};
-  sa.sa_sigaction = sigusr2_handler;
-  sa.sa_flags = SA_SIGINFO | SA_RESTART;
-  sigemptyset(&sa.sa_mask);
-  sigaction(SIGUSR2, &sa, nullptr);
-}
-
 static void interrupt_handler(int signo) {
+  std::array<char, 512> msg{};
+  const auto pid = getpid();
+  const auto *signal_name = (signo == SIGINT) ? "SIGINT" : ((signo == SIGTERM) ? "SIGTERM" : "SIGNAL");  // NOLINT
+
+  snprintf(msg.data(), msg.size(), "[RECEIVED %s] Sending SIGUSR2 to all threads in process %d",
+           signal_name,
+           (int)pid);
+  log_message(msg.data());
+
   // Send SIGUSR2 to all threads (iterate /proc/self/task)
-  pid_t pid = getpid();
   DIR *d = opendir("/proc/self/task");
   if (d != nullptr) {
     struct dirent *ent = nullptr;
+    int thread_count = 0;
     while ((ent = readdir(d)) != nullptr) {
       if (ent->d_name[0] == '.') {
         continue;
@@ -94,17 +111,40 @@ static void interrupt_handler(int signo) {
       if (tid <= 0) {
         continue;
       }
-      syscall(SYS_tgkill, pid, tid, SIGUSR2);
+      const auto ret = (int)syscall(SYS_tgkill, pid, tid, SIGUSR2);
+      thread_count++;
+      std::array<char, 256> thread_msg{};
+      snprintf(thread_msg.data(), thread_msg.size(), "[SIGNAL SENT] SIGUSR2 -> TID %d (result=%d)",
+               (int)tid, ret);
+      log_message(thread_msg.data());
     }
     closedir(d);
+
+    std::array<char, 256> count_msg{};
+    snprintf(count_msg.data(), count_msg.size(), "[SIGNAL BROADCAST] Sent SIGUSR2 to %d threads", thread_count);
+    log_message(count_msg.data());
   }
 
   // give threads a moment to print their traces
+  log_message("[HANDLER] Sleeping 1 second to allow threads to print backtraces...");
   sleep(1);
 
   // restore default and re-raise to allow normal termination
+  std::array<char, 256> exit_msg{};
+
+  snprintf(exit_msg.data(), exit_msg.size(), "[HANDLER] Restoring default handler and re-raising %s", signal_name);
+  log_message(exit_msg.data());
   signal(signo, SIG_DFL);
   raise(signo);
+}
+
+static void install_backtrace_handler() {
+  struct sigaction sa{};
+  sa.sa_sigaction = sigusr2_handler;
+  sa.sa_flags = SA_SIGINFO | SA_RESTART;
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGUSR2, &sa, nullptr);
+  log_message("[HANDLER INSTALL] SIGUSR2 handler installed");
 }
 
 static void install_interrupt_handlers() {
@@ -114,6 +154,7 @@ static void install_interrupt_handlers() {
   sigemptyset(&sa.sa_mask);
   sigaction(SIGINT, &sa, nullptr);
   sigaction(SIGTERM, &sa, nullptr);
+  log_message("[HANDLER INSTALL] SIGINT and SIGTERM handlers installed");
 }
 
 // NOLINTEND(*array-index*, *vararg)
